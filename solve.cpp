d01c5cb8cbb87c312b77689fedf7f675275862d3
Hasibur Rahman <hasiburdev@gmail.com>
2022-08-09 17:08:18 +0600

    Changed file i/o for CP Editor

--- solve.cpp
+++ solve.cpp
@@ -7,34 +7,36 @@ using namespace std;
 #define S second
 #define PB push_back
 #define all(p) p.begin(), p.end()
-#define optimize()              \
-  ios_base::sync_with_stdio(0); \
-  cin.tie(0);                   \
+#define optimize()                                                             \
+  ios_base::sync_with_stdio(0);                                                \
+  cin.tie(0);                                                                  \
   cout.tie(0);
 #define endl "\n"
 #define sz(x) (int)x.size()
-#define fraction(x)             \
-  cout.unsetf(ios::floatfield); \
-  cout.precision(x);            \
+#define fraction(x)                                                            \
+  cout.unsetf(ios::floatfield);                                                \
+  cout.precision(x);                                                           \
   cout.setf(ios::fixed, ios::floatfield);
+
+#ifndef ONLINE_JUDGE
+#define file()                                                                 \
+  freopen("input.txt", "r", stdin);                                            \
+  freopen("output.txt", "w", stdout);
+#else
+#define file()
+#endif
 
 const double eps = 1e-9;
 const double PI = acos(-1);
 
 void solve();
 
-int main()
-{
+int main() {
   optimize();
-
-#ifndef ONLINE_JUDGE
-  freopen("input.txt", "r", stdin);
-  freopen("output.txt", "w", stdout);
-#endif
 
   clock_t start_time = clock();
   int t = 1;
-  cin >> t;
+  // cin >> t;
   while (t--)
     solve();
   clock_t end_time = clock();
@@ -43,6 +45,4 @@ int main()
   cerr << "Run Time: " << run_time << "ms" << endl;
 }
 
-void solve()
-{
-}
+void solve() {}
