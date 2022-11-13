ad7cefeab051e6fc457aae99c996e0aa46121264
Hasibur Rahman <hasiburdev@gmail.com>
2022-11-14 00:54:07 +0600

    Update solve.js

--- solve.js
+++ solve.js
@@ -1,25 +1,16 @@
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
-
 let inputString = '';
 let currentLine = 0;
+const readLine = _ => inputString[currentLine++];
 
-const readLine = () => {
-    return inputString[currentLine++];
-}
-
-const main = () => {
+const main = _ => {
 // solution goes here....
 
 }
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
+process.stdin.on('data', inputStdin => inputString += inputStdin);
 process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
+    inputString = inputString.trim().split('\n').map(string => string.trim());
     main();  
 });
