a5dbcc7f89e94be0bc7317691bacd3f9e1baa8ed
Hasibur Rahman <hasiburdev@gmail.com>
2022-11-25 21:04:11 +0600

    Update solve.cpp

--- solve.cpp
+++ solve.cpp
@@ -4,55 +4,43 @@ using namespace std;
 
 typedef long long ll;
 
+#define endl '\n';
+
 void solve();
 
-int main()
-{
-
-    int tc = 1;
-    cin >> tc;
-    while (tc--)
-    {
-        solve();
-    }
+int main() {
+  int tc = 1;
+  cin >> tc;
+  while (tc--) {
+    solve();
+  }
 }
 
-void solve()
-{
-    ll n, mx = 0, mxSecond = 0, mxIndex = 0;
-    cin >> n;
-    ll numbers[n];
-    for (ll i = 0; i < n; i++)
-    {
-        ll in;
-        cin >> in;
-        numbers[i] = in;
-        mxIndex = mx != max(mx, in) ? i : mxIndex;
-        ll temp = mx;
-        // cout << temp << " t s " << mxSecond << endl;
-        mx = max(mx, in);
-
-        if (mx != temp)
-            mxSecond = temp;
-        else
-        {
-            mxSecond = max(mxSecond, in);
-        }
+void solve() {
+  ll n, mx = 0, mxSecond = 0, mxIndex = 0;
+  cin >> n;
+  ll numbers[n];
+  for (ll i = 0; i < n; i++) {
+    ll in;
+    cin >> in;
+    numbers[i] = in;
+    mxIndex = mx != max(mx, in) ? i : mxIndex;
+    ll temp = mx;
+    mx = max(mx, in);
+    if (mx != temp)
+      mxSecond = temp;
+    else {
+      mxSecond = max(mxSecond, in);
     }
+  }
 
-    if (mxSecond == 0)
-        mxSecond = mx;
-
-    // cout << mxSecond << endl;
-    for (ll i = 0; i < n; i++)
-    {
-        // cout << "I:: " << i << endl;
-        // cout << "idx::" << mxIndex << endl;
-
-        if (i == mxIndex)
-            cout << numbers[i] - mxSecond << " ";
-        else
-            cout << numbers[i] - mx << " ";
-    }
-    cout << endl;
+  if (mxSecond == 0)
+    mxSecond = mx;
+  for (ll i = 0; i < n; i++) {
+    if (i == mxIndex)
+      cout << numbers[i] - mxSecond << " ";
+    else
+      cout << numbers[i] - mx << " ";
+  }
+  cout << endl;
 }
---
+++ templates/template.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+
+
+void solve();
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int test_case = 1;
+    // cin >> test_case;
+    while(test_case--) solve();
+    return 0;
+}
+
+void solve() {
+    
+}
