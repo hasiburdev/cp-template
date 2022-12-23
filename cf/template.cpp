0ecf22435c0b2a55ab4c4bda383887f7e8b2d735
Hasibur Rahman <hasiburdev@gmail.com>
2022-12-23 21:48:37 +0600

    Add template file for cf-tool

---
+++ cf/template.cpp
@@ -0,0 +1,20 @@
+// Username: $%U%$
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n';
+
+void solve();
+
+int32_t main() {
+  int tc = 1;
+  // cin >> tc;
+  while (tc--) solve();
+  return 0;
+}
+
+void solve() {
+ 
+}
+
